a8a71e764538e6e9d82ff5d034862fd5fde52c65
akwright <alexander.k.wright@gmail.com>
2015-09-04 21:32:26 -0400

    Added smooth scrolling for the nav links. Added jQuery into the project.

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -10,47 +10,74 @@
 
   'use strict';
 
+  function getDomNodeArray(domNodeList) {
+    var arrayFromNodeList = [];
+
+    for (var k = 0; k < domNodeList.length; k++) {
+      arrayFromNodeList.push(domNodeList[k]);
+    }
+
+    return arrayFromNodeList;
+  }
+
   (function () {
     var mobileTrigger  = document.querySelector('.js-nav-toggle'),
         mobileNav      = document.querySelector('.js-nav'),
         inputs         = document.querySelectorAll('.input'),
-        inputOnClass   = "is-active",
-        inputShowClass = "is-visible";
+        inputArr       = getDomNodeArray(inputs),
+        inputOnClass   = 'is-active',
+        inputShowClass = 'is-visible';
 
-    mobileTrigger.addEventListener("click", function() {
+    mobileTrigger.addEventListener('click', function() {
       mobileNav.classList.toggle('is-visible');
     });
 
-    function toggleInputs(ele) {
-      var label = ele.previousSibling;
+    function toggleInputs() {
+      var label = this.previousSibling;
 
-      if (ele.value !== "") {
+      if (this.value !== '') {
         label.classList.add(inputShowClass);
       } else {
         label.classList.remove(inputShowClass);
       }
     }
 
-    for (var i = 0; i < inputs.length; i++) {
-      inputs[i].addEventListener("keyup", function() {
-        toggleInputs(this);
-      });
-
-      inputs[i].addEventListener("focus", function() {
-        var label = this.previousSibling;
-        console.log(label);
-        console.log(this);
+    function toggleLabelClass(event) {
+      var label = this.previousSibling;
+      if (event.type === 'focus') {
         label.classList.add(inputOnClass);
-      });
-
-      inputs[i].addEventListener("blur", function() {
-        var label = this.previousSibling;
+      } else {
         label.classList.remove(inputOnClass);
-      });
+      }
+    }
 
-      toggleInputs(inputs[i]);
+    for (var i = 0; i < inputArr.length; i++) {
+      inputArr[i].addEventListener('keyup', toggleInputs, false);
+
+      inputArr[i].addEventListener('focus', toggleLabelClass, false);
+
+      inputArr[i].addEventListener('blur', toggleLabelClass, false);
     }
 
   })();
 
 })(window, document);
+
+
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    var $links = $('.js-nav a');
+
+    $links.each(function() {
+      $(this).on('click', function(e) {
+        $('html, body').stop().animate({ scrollTop: $($(this).attr('href')).offset().top }, 500);
+        e.preventDefault();
+      });
+    });
+
+  });
+
+})(jQuery, window, document);
